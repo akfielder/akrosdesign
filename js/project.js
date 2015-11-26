d3b9a942130dce8dd02ec9e8c29c8a3d5b541234
Lexi Fielder <alexandra.fielder@gmail.com>
2015-11-26 11:29:12 +0000

    Updated project.js

--- js/project.js
+++ js/project.js
@@ -1,30 +1,32 @@
 
+// Email Validation
+$(document).ready(function() {
 
-$('#btn-submit').click(function() {
+  $('#btn-submit').click(function() {
 
-  $(".error").hide();
-  var hasError = false;
-  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    $(".error").hide();
+    var hasError = false;
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
 
-  var emailaddressVal = $("#contact-email").val();
-  if(emailaddressVal == '') {
-    $("#contact-email").after('Please enter your email');
-    $("#contact-email").css('color', 'red');
-    hasError = true;
-  }
+    var emailaddressVal = $("#contact-email").val();
+    if(emailaddressVal == '') {
+      $("#contact-email").after('Please enter your email');
+      $("#contact-email").css('border-color', 'red');
+      hasError = true;
+    }
 
 var messageVal = $("#message").val();
 if (messageVal == '') {
-$("#message").after('Send us a message');
-$("#message").css('color', 'red');
-hasError = true;
+  $("#message").after('Send us a message');
+  $("#message").css('border-color', 'red');
+  hasError = true;
 }
 
-  if(hasError == true) { return false; }
+    if(hasError == true) { return false; }
 
-  });
-
+    });
+});
 
 $(".trigger").on("click", function (){
   // use the jquery slide toggle function to animate in and out our navigation links
